1466e6dff1a48b0a211d49755cf879a187571fef
JohnDoe1940 <johndoe1940c@proton.me>
2025-07-09 20:35:03 +0200

    jj
    jj

---
+++ customScripts.js
@@ -0,0 +1,39 @@
+﻿function downloadFile(fileDataUrl) {
+    if (!fileDataUrl) {
+        return;
+    }
+
+    const parts = fileDataUrl.split(',');
+    const base64Data = parts[1];
+    const headerParts = parts[0].split(';');
+    const mimeType = headerParts[0].split(':')[1];
+    const fileName = headerParts[1].split(':')[1];
+
+    const byteCharacters = atob(base64Data);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+        const slice = byteCharacters.slice(offset, offset + 512);
+        const byteNumbers = new Array(slice.length);
+
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, {type: mimeType});
+    const url = window.URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    window.URL.revokeObjectURL(url);
+}
